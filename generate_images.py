1c1bc13a24490daac362999a4cc64ed347e69ff5
Alba Minguez <minguezalba@gmail.com>
2019-07-26 23:50:45 +0200

    Improved prints loop

--- generate_images.py
+++ generate_images.py
@@ -14,19 +14,18 @@ def generate_images(method_image):
     audio_files = read_directory(DIR_AUDIOS, EXT_AUDIOS)
     label_files = read_directory(DIR_LABELS, EXT_LABELS)
 
-    for (f_path, f_name), (l_path, l_name) in zip(audio_files, label_files):
-
-        print(f'Loading audio file {f_name}')
+    for f, ((f_path, f_name), (l_path, l_name)) in enumerate(zip(audio_files, label_files)):
+        print(f'Loading audio file {f+1}/{len(audio_files)}: {f_name}')
         audio_manager = AudioManager.load_audio(f_path, f_name)
         labels_manager = LabelsManager.load_labels(l_path, l_name)
 
         for i, (audio_frame, label) in enumerate(zip(audio_manager.data, labels_manager.data)):
-            print(f'\t - Frame {i+1} / {audio_manager.n_frames}', flush=True)
+            print(f'\t - Frame {i+1} / {audio_manager.n_frames}')
             image_manager = ImageManager.generate_image(method_image, audio_frame, i, f_name, label)
             image_manager.save_image(method_image, label, show=False)
             # AudioManager.show_audio(audio_frame, audio_manager.fs, image_manager.filename)
 
 
 if __name__ == '__main__':
-    method_image_ = 'greyscale'  # RBG or greyscale
+    method_image_ = 'RGB'  # RBG or greyscale
     generate_images(method_image_)
