0fbf3ffcc36b8cb57e49e458259b122710d28819
Alba Minguez <minguezalba@gmail.com>
2019-08-07 23:03:47 +0200

    New experiment until layer 7

--- 3_image_model_testing.py
+++ 3_image_model_testing.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     start = time.time()
 
     image_method_ = 'RGB/oversampling/'
-    filepath_ = 'data/models/2019-08-06T07:00:22.659687.pth'
+    filepath_ = 'data/models/2019-08-06T22:21:47.328350.pth'
     main(image_method_, filepath_)
 
     end = time.time()
